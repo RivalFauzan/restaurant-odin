51cb481740d07a1fd11432b9eedb843244ab1627
rivalfauzan <rfauzanj31@gmail.com>
2026-09-23 08:42:26 +0700

    memisahkan file webpack.config.js menjadi common, dev, dan prod

--- webpack.common.js
+++ webpack.common.js
@@ -1,8 +1,8 @@
+// webpack.common.js
 import path from "node:path";
 import HtmlWebpackPlugin from "html-webpack-plugin";
 
 export default {
-  mode: "development",
   entry: "./src/index.js",
   output: {
     filename: "main.js",
@@ -16,18 +16,18 @@ export default {
   ],
   module: {
     rules: [
-        {
-            test: /\.css$/i,
-            use: ["style-loader", "css-loader"],
-        },
-        {
-            test: /\.html$/i,
-            use: ["html-loader"],
-        },
-        // {
-        //     test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        //     type: "asset/resource",
-        // }
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.html$/i,
+        use: ["html-loader"],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: "asset/resource",
+      }
     ],
   },
 };
